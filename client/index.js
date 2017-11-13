6078bb42f61c977f957d8e9507d22d22be9b2d22
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-13 13:12:11 -0800

    cleaned up the commented out lines.

--- client/index.js
+++ client/index.js
@@ -1,20 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-// render(<div>Hi, welcome to React!</div>, document.querySelector('#app'))
-
-// function update() {
-//   const date = new Date()
-//   const element = (
-//     <div>
-//       Current time is: {date.getHours()}: {date.getMinutes()}: {date.getSeconds()}
-//     </div>
-//   )
-//   render(element, document.querySelector('#app'))
-// }
-//
-// window.setInterval(update, 1000)
-
 function GetTime(props) {
   return <div>Current time is: {props.time}</div>
 }
