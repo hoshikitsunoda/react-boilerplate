39459455a5b74741742cd35e956903c36ac9bf97
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-13 13:49:02 -0800

    used class constructor.

--- client/index.js
+++ client/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-function GetTime(props) {
-  return <div>Current time is: {props.time}</div>
+// function GetTime(props) {
+//   return <div>Current time is: {props.time}</div>
+// }
+
+const style = {
+  border: '5px solid tomato',
+  textAlign: 'center',
+  padding: '100px',
+  fontSize: '30px',
+  color: 'pink',
+  textShadow: '2px 2px 8px grey',
+  borderRadius: '50%',
+  boxShadow: 'inset 10px 10px 50px tomato'
+}
+
+class GetTime extends React.Component {
+  render() {
+    return <div style={style}>Current time is: {this.props.time}</div>
+  }
 }
 
 function tick() {
