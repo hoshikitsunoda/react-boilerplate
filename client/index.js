ba9f7d7b6e9a281e0587f2568b76ed44d6127682
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-13 12:49:39 -0800

    Current time with React Components and Props.

--- client/index.js
+++ client/index.js
@@ -1,4 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-render(<div>Hi, welcom to React!</div>, document.querySelector('#app'))
+// render(<div>Hi, welcome to React!</div>, document.querySelector('#app'))
+
+// function update() {
+//   const date = new Date()
+//   const element = (
+//     <div>
+//       Current time is: {date.getHours()}: {date.getMinutes()}: {date.getSeconds()}
+//     </div>
+//   )
+//   render(element, document.querySelector('#app'))
+// }
+//
+// window.setInterval(update, 1000)
+
+function GetTime(props) {
+  return <div>Current time is: {props.time}</div>
+}
+
+function tick() {
+  const current = new Date().toLocaleString()
+  const time = <GetTime time={current} />
+
+  render(time, document.querySelector('#app'))
+}
+
+window.setInterval(tick, 1000)
